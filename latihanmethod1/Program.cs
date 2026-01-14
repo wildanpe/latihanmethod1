075d67cae2b38ef8a95430b41ad2986d1fcacc3b
wildanpe <bisei569@gmail.com>
2026-01-14 14:28:07 +0700

    latihanmethod1

--- latihanmethod1/Program.cs
+++ latihanmethod1/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("apalah");
+﻿int a = 10, b = 2, c = 0;
+
+void tambah ()
+{
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+
+void kurang()
+{
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+
+void kali()
+{
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+
+void bagi()
+{
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+tambah();
+kurang();
+kali();
+bagi();
